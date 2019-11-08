454b15f7eb981a62b5ed89eb01a81e6e7ff4caf4
Jiwook Kim <j533kim@edu.uwaterloo.ca>
2019-11-08 11:54:14 +0900

    updated

--- about-studying.js
+++ about-studying.js
@@ -1,124 +1,103 @@
-/*document.addEventListener("DOMContentLoaded", function(event) {
-    fetch('./json/gpa.json').then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        var parsedData = parseData(data);
-        drawChart(parsedData);
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
+var schoolContainer = $("#school-container");
+var schools;
+const category = ["duration","region","program"];
+
+function renderContext(num = 0) {
+    schools.forEach(function (school) {
+        var parentRow = $("#school-id-" + school.id);
+        var downDiv = $("<div></div>").addClass("col-sm-8");
+        var lan = num == 0 ? "en" : "kr";
+        var title = $("<h4></h4>").addClass("schoolTitle").text(school.name[lan]),
+            label,
+            dur;
+            
+        downDiv.append(title);
+        
+        category.forEach(function(cate) {
+            if (school[cate][lan] != "") {
+                switch (cate) {
+                    case "duration":
+                        label = num == 0 ? "Duration: " : "기간: ";
+                        break;
+                    case "region":
+                        label = num == 0 ? "Region: " : "지역: ";
+                        break;
+                    default: // "program"
+                        label = num == 0 ? "Program: " : "전공: ";
+                }
+                dur = $("<p></p>").addClass("exp").text(school[cate][lan]);
+                $enhanceLabel = $("<b></b>").text(label);
+                dur.prepend($enhanceLabel);
+                downDiv.append(dur);
+            }
+        });
+        dur = $("<ul></ul>");
+        for (var i = 0; i < school.description[lan].length; ++i) {
+            var _lst = $("<li></li>").addClass("bullet-points")
+                .text(school.description[lan][i]).css("text-align", "left");
+            dur.append(_lst);
+        }
+        downDiv.append(dur);
+        parentRow.append(downDiv);
+    });
+}
+
+function setImageInterval(imgContents, eachSchool) {
+    var timeInterval = 8000; // 8s
+    var imgItself = imgContents.find(".image-container > .image-itself");
+    var imgCred = imgContents.find(".image-credit > #reference");
+    window.setInterval(function() {
+        var index = eachSchool.images.map(function(i) { return i.src; }).indexOf(imgItself.attr("src"));
+        var len = eachSchool.images.length;
+        if (len == index + 1) index = -1;
+        imgItself.fadeOut(function() {
+            $(this).attr("src", eachSchool.images[index + 1].src).fadeIn();
+        });
+        imgCred.fadeOut(function() {
+            $(this).text(eachSchool.images[index + 1].credit).fadeIn();
+        });
+    }, timeInterval);
+}
+
+fetch('json/education.json').then((response) => {
+    return response.json();
+}).then((data) => {
+    schools = data.schools;
+    schools.forEach(function (school) {
+        var imgRef = $("<p></p>").attr("id", "reference").text(school.images[0].credit);
+        var imgCredit = $("<div></div>").addClass("image-credit");
+        imgCredit.append(imgRef);
+        
+        var img = $("<img>").addClass("image-itself").attr("src",school.images[0].src).css("width","100%").attr("alt",school.name.en);
+        var imgContainer = $("<div></div>").addClass("image-container");
+        imgContainer.append(img);
+
+        var upDiv = $("<div></div>").addClass("col-sm-4");
+        upDiv.append(imgContainer);
+        upDiv.append(imgCredit);
+
+        setImageInterval(upDiv, school);
+        
+        var parentRow = $("<div></div>").addClass("row").attr("id", "school-id-" + school.id);
+        parentRow.append(upDiv);
+        schoolContainer.append(parentRow);
+    });
+    if (window.location.href.slice(-3) == "kor") {
+        renderContext(1);
+    } else {
+        renderContext();
+    }
+}).catch(function (error) {
+    console.log(error);
 });
 
-function parseData(data) {
-    var arr = [];
-    for (var i in data.highSchool) {
-        arr.push({
-            term: new term(i),
-            value: data.highSchool[i].gpa
-        });
+$("#lang-setting").on('change', function() {
+    schools.forEach(function(school) {
+        $("#school-id-" + school.id).children().last().remove();
+    });
+    if ($(this).is(':checked')) { // Korean
+        renderContext(1);
+    } else { // English
+        renderContext();
     }
-    return arr;
-}
-
-function drawChart(data) {
-var svgWidth = 600, svgHeight = 400;
-var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-var svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
-    
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var x = d3.scaleTime()
-    .rangeRound([0, width]);
-
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-var line = d3.line()
-    .x(function(d) { return x(d.term)})
-    .y(function(d) { return y(d.value)})
-    x.domain(d3.extent(data, function(d) { return d.term }));
-    y.domain(d3.extent(data, function(d) { return d.value }));
-
-g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .select(".domain")
-    .remove();
-
-g.append("g")
-    .call(d3.axisLeft(y))
-    .append("text")
-    .attr("fill", "#000")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "0.71em")
-    .attr("text-anchor", "end")
-    .text("Price ($)");
-
-g.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
-}
-*/
-
-var num = 2,
-    dae_num = 2;
-window.setInterval(function() {
-    const default_credit = "Map data © ",
-        you = "Yousef Shabana",
-        cairoC = "Cairo American College",
-        goo = " Google | ",
-        s = "src",
-        i = "-image",
-        j = ".jpg",
-        direct = "./images/schools/";
-
-    var src_waterloo = direct + "waterloo_" + dae_num.toString() + j,
-        src_cac = direct + "cac_" + dae_num.toString() + j,
-        src_pureun = direct + "pureun_" + num.toString() + j,
-        src_ishcmc = direct + "ishcmc_" + num.toString() + j,
-        src_sis = direct + "sis_" + num.toString() + j,
-        src_daepyong = direct + "daepyong_" + dae_num.toString() + j;
-    
-    $("#waterloo" + i).fadeOut(function() {
-        $(this).attr(s, src_waterloo).fadeIn();
-    });
-    
-    $("#cac" + i).fadeOut(function() {
-        $(this).attr(s, src_cac).fadeIn();
-    });
-
-    $("#reference-cairo").fadeOut(function() {
-        if (dae_num == 2) $(this).text(default_credit.concat("2016" + goo + you)).fadeIn();
-        else $(this).text(default_credit.concat("2015" + goo + cairoC)).fadeIn();
-    });
-    
-    $("#pureun" + i).fadeOut(function() {
-        $(this).attr(s, src_pureun).fadeIn();
-    });
-    $("#ishcmc" + i).fadeOut(function() {
-        $(this).attr(s, src_ishcmc).fadeIn();
-    });
-    $("#sis" + i).fadeOut(function() {
-        $(this).attr(s, src_sis).fadeIn();
-    });
-    $("#daepyong" + i).fadeOut(function() {
-        $(this).attr(s, src_daepyong).fadeIn();
-    });
-    num++;
-    dae_num++;
-    if (num == 4) num = 1;
-    if (dae_num == 3) dae_num = 1;
-}, 8000);
+});
